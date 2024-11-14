21dcfe8c5e48d0d8a12a58e45314c95ba7e4b505
just-codingbaby <gocks5393@naver.com>
2024-11-14 16:16:51 +0900

    마이그레이션 파일 삭제

--- prisma/migrations/20241111093914_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `img` to the `StartUp` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "StartUp" ADD COLUMN     "img" TEXT NOT NULL;
--- prisma/migrations/20241112045202_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `img` on the `StartUp` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "StartUp" DROP COLUMN "img";
